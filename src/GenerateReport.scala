d63febaedf6d3a4926f857a6bb32f557b09e7e0f
Rafal Rawicki <rafal@rawicki.org>
2010-10-18 18:13:48 +0200

    Encapsulate statistics in Stats class.

--- src/GenerateReport.scala
+++ src/GenerateReport.scala
@@ -3,35 +3,40 @@ import surveys.DataImporter.DataImporter
 import surveys.HTMLGenerator.HTMLGenerator
 
 object GenerateReport {
-  def personMeanAndDevation(xs: List[Answers]): Map[Person, Map[String, (Double, Double, Int)]] = {
-      def meanDevationAndSampleSize(xs: List[Int]): (Double, Double, Int) = {
-          val mean = (xs.sum: Double) / xs.size
-          val variance = xs.foldLeft(0: Double) {
-              case (acc, x) => ((x-mean)*(x-mean): Double)/xs.size + acc
-          }
-          (mean, scala.math.sqrt(variance), xs.size)
+  case class Stats(mean: Double, dev: Double, med: Double, sample_size: Int)
+
+  def getStats(xss: List[Answer]): Stats = {
+      val xs = xss map (_.value)
+      val mean = (xs.sum: Double) / xs.size
+      val variance = xs.foldLeft(0: Double) {
+          case (acc, x) => ((x-mean)*(x-mean): Double)/xs.size + acc
       }
+      val med = xs.sorted.apply(xs.size / 2)
+      Stats(mean, scala.math.sqrt(variance), med, xs.size)
+  }
+
+  def personMeanAndDevation(xs: List[Answers]): Map[Person, Map[String, Stats]] = {
       val perPerson: Map[Person, List[Answers]] = xs groupBy (_.person)
       perPerson mapValues { xs =>
           val answers: List[Answer] = xs flatMap (_.values)
           val questions: Map[String, List[Answer]] = answers groupBy (_.question.value)
-          questions mapValues (xs => meanDevationAndSampleSize(xs map (_.value)))
+          questions mapValues getStats
       }
   }
 
   def main(args: Array[String]){
     val answers = DataImporter.readAnswers
-		val xs: List[(Person, String, (Double, Double, Int))] = for {
+		val xs: List[(Person, String, Stats)] = for {
 			(person, qs) <- personMeanAndDevation(answers).toList
-			(question, (mean, devation, size)) <- qs.toList
-		} yield (person, question, (mean, devation, size))
+			(question, stats) <- qs.toList
+		} yield (person, question, stats)
         val xss = xs filter {
-            case (_, question, (_, _, _)) => question.startsWith("Og")
+            case (person, question, _) => person.name.startsWith("Szy") && person.lastName.startsWith("Ac")
         }
 		val byDevation = xss sortBy {
-			case (_, _, (mean, devation, _)) => -mean
+			case (_, _, Stats(mean, devation, _, _)) => -mean
 		}
-		for ((person, question, (mean, devation, size)) <- byDevation take 30) {
+		for ((person, question, Stats(mean, devation, med, size)) <- byDevation take 30) {
 			print(person)
             print(" " * (45 - person.toString().length))
 			println("Mean: %1f,\tDev: %2f \t(%d)\tand Question '%3s'".format(mean, devation, size, question))
