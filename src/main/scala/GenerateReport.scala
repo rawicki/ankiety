7502d476bce20f132102637434f4070030f4e503
Rafal Rawicki <rafal@rawicki.org>
2010-12-12 21:56:41 +0100

    Add generating separate reports for Mathematics and Computer Science.

--- src/main/scala/GenerateReport.scala
+++ src/main/scala/GenerateReport.scala
@@ -1,18 +1,26 @@
 import java.io.{OutputStreamWriter, FileOutputStream}
 import scala.xml._
 
+import surveys.SurveyClasses.Survey
 import surveys.DataImporter.DataImporter
 import surveys.ReportBuilder.ReportBuilder
 
 object GenerateReport {
+  def generateReport(answers: List[Survey], title: String) {
+    val report = ReportBuilder.buildReport(answers)
+
+    val fw = new OutputStreamWriter(new FileOutputStream(title + ".html"), "UTF-8")
+    fw.write(report.toString)
+    fw.close()
+  }
+
   def main(args: Array[String]){
     val salt = if (args contains "md5") Some((1 to 10).map(_ => scala.util.Random.nextPrintableChar).mkString("")) else None
     val answers = (new DataImporter(salt)).readSurveys
 
-    val report = ReportBuilder.buildReport(answers)
 
-    val fw = new OutputStreamWriter(new FileOutputStream("Report.html"), "UTF-8")
-    fw.write(report.toString)
-    fw.close()
+    generateReport(answers, "Report")
+    generateReport(answers.filter(_.clazz.subject.code.startsWith("1000-1")), "Mathematics")
+    generateReport(answers.filter(_.clazz.subject.code.startsWith("1000-2")), "ComputerScience")
   }
 }
