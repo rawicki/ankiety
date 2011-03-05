40f456662c4ff4023693dd1c4861dfaf442d4488
Rafal Rawicki <rafal@rawicki.org>
2011-03-05 18:19:54 +0100

    Remove unwanted statistics from Publishing Report.

--- src/main/scala/PublishingReport.scala
+++ src/main/scala/PublishingReport.scala
@@ -57,48 +57,11 @@ class PublishingReport(answers: List[Survey], categorization: Categorization)
             }
           </div>
           <div class="center">
-            <h2>Średni wynik dla wszystkich pytań wg stopnia lub tytułu naukowego</h2>
-            { show_per_category_stats(statsByTitle, "Stopień/Tytuł") }
-          </div>
-          <div class="center">
-            <h2>Średni wynik dla wszystkich pytań wg rodzaju stanowiska</h2>
-            { show_per_category_stats(statsByAggregatedPosition, "Rodzaj stanowiska") }
-          </div>
-          <div class="center">
-            <h2>Średni wynik dla wszystkich pytań wg stanowiska</h2>
-            { show_per_category_stats(statsByPosition, "Stanowisko") }
-          </div>
-          <div class="center">
-            <h2>Średni wynik dla wszystkich pytań wg typu zajęć</h2>
-            { show_per_category_stats(statsByClassType, "Typ zajęć") }
-          </div>
-          <div class="center">
             <h2>Najlepsze wyniki (osoba, przedmiot)</h2>
             {
               implicit val ord = Ordering.by[ClassStats, Double](_.quality.mean).reverse
               showCategorized(statsByPersonSubject, _.title(rankingPercent), show_per_person_stats(_, rankingPercent,
                 None), categorization)
-            }
-          </div>
-          <div class="center">
-            <h2>{rankingPercent}% najgorszych wyników (osoba, przedmiot)</h2>
-            {
-              implicit val ord = Ordering.by[ClassStats, Double](_.quality.mean)
-              show_per_person_stats(statsByPersonSubject, rankingPercent, comments)
-            }
-          </div>
-          <div class="center">
-            <h2>{rankingPercent}% najbardziej kontrowersyjnych wyników (osoba, przedmiot)</h2>
-            {
-              implicit val ord = Ordering.by[ClassStats, Double](_.quality.dev).reverse
-              show_per_person_stats(statsByPersonSubject, rankingPercent, comments)
-            }
-          </div>
-          <div class="center">
-            <h2>{rankingPercent}% najczęściej opuszczanych zajęć (osoba, przedmiot)</h2>
-            {
-              implicit val ord = Ordering.by[ClassStats, Double](_.attendance.mean)
-              show_per_person_stats(statsByPersonSubject, rankingPercent, comments)
             }
           </div>
           <div class="center">
