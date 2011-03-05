ab5f96c8e0492e63e312978905439f2b6ee73b06
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-03-05 15:25:06 +0100

    Added intellij plug-in.
    Signed-off-by: Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>

--- project/build/AnkietyProject.scala
+++ project/build/AnkietyProject.scala
@@ -1,7 +1,7 @@
 
 import sbt._
 
-class AnkietyProject(info: ProjectInfo) extends DefaultProject(info) {
+class AnkietyProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
 
   val javacsv = "net.sourceforge.javacsv" % "javacsv" % "2.0"
   
