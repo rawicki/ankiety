6afe473ac64d19dd3b5dac52b66585238f7abfa5
Rafal Rawicki <rafal@rawicki.org>
2011-04-07 21:22:31 +0200

    Move ranking description to the last section.

--- src/main/scala/CompleteReport.scala
+++ src/main/scala/CompleteReport.scala
@@ -128,6 +128,8 @@ class CompleteReport(answers: List[Survey], categorization: Categorization, peri
             { scatterPlot(commentsFilled zip relativeFilled, 1) }
           </div>
           <div class="note" id="legend">
+            <h2>Legenda</h2>
+            <img src="templates/description.png" alt="Opis rankingów" />
             <p>Rankingi są sporządzane według zasady, że odrzucamy oceny, dla których próbka jest mniejsza od 5. Następnie
             oceny są sortowane po ogólnej ocenie i odcinane jest 25% najlepszych wyników. Jeśli na granicy odcięcia
             oceny są takie same, próg odcięcia jest przesuwany do pierwszej oceny niżej w rankingu.</p>
